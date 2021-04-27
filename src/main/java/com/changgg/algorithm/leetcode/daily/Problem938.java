57b6a80c21c68dd168e6b01c7bf2c69009d0e084
changgg <891842749@qq.com>
2021-04-27 23:23:54 +0800

    【leetcode日常】20210427

---
+++ src/main/java/com/changgg/algorithm/leetcode/daily/Problem938.java
@@ -0,0 +1,31 @@
+package com.changgg.algorithm.leetcode.daily;
+
+/**
+ * @author changgg
+ * @Classname Problem938
+ * @Description https://leetcode-cn.com/problems/range-sum-of-bst/
+ * @Date 2021/4/27 23:18
+ */
+public class Problem938 {
+    private int rs = 0;
+
+    public int rangeSumBST(Problem173.TreeNode root, int low, int high) {
+        this.dsf(root, low, high);
+        return rs;
+    }
+
+    private void dsf(Problem173.TreeNode root, int low, int high) {
+        if (root == null) {
+            return;
+        }
+        dsf(root.left, low, high);
+        if (root.val >= low && root.val <= high) {
+            rs += root.val;
+        }
+        dsf(root.right, low, high);
+    }
+
+    public static void main(String[] args) {
+        Problem938 problem938 = new Problem938();
+    }
+}
